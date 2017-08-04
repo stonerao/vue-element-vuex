1f9450b3225796b6502536485138ef084a0c0af3
674656681@qq.com <674656681@qq.com>
2017-08-04 18:21:00 +0800

    kd
    Signed-off-by: 674656681@qq.com <674656681@qq.com>

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,9 @@
 export const api = { 
     teacher_list:'/Admin/teacher/teacher_list',//teacher list data
+    teacher_kemu:'/Admin/teacher/teacher_kemu',//teacher 科目列表
+    teacher_add:'/Admin/teacher/teacher_add',//增加老师
+    teacher_edit:'/Admin/teacher/teacher_edit',//编辑老师
+    teacher_delete:'/Admin/teacher/teacher_delete',//删除老师
+    studentlist:'/Student/SchoolStudent/studentlist',//学生列表
+    department_list:'/Admin/Department/department_list',//所有组织结构关系表
 }
---
+++ src/api/layout.js
@@ -0,0 +1,3 @@
+export const api = { 
+    header:'Admin/Store/member_info',//头部信息
+}
