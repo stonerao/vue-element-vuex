0b765e820cc1cc34944e1d68498e715fd7c1f605
LTK <403182090@qq.com>
2017-08-14 18:26:29 +0800

    3213212131

--- src/api/grade.js
+++ src/api/grade.js
@@ -2,4 +2,7 @@ export const api = {
     gradeSearch:'Admin/Grade/get_zdepartment_list',  //年或班级管理之总课表数据
     timeTable:'Admin/Grade/class_list',  //年或班级管理之总课表数据
     scheduleData:'Admin/Grade/get_class_grade',  //总课表处点击排课后进行排课初始数据获取
+    subjectData:'Admin/Grade/get_subject',  //科目总数据
+    teacherData:'Admin/Grade/get_teacher',  //老师总数据
+    scheduleSave:'Admin/Grade/class_add',  //课表添加保存提交
 }
