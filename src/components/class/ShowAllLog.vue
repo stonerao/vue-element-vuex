6626d719f61034e60e2fa415ed8f4c33b02d70f3
LTK <403182090@qq.com>
2017-08-25 11:01:59 +0800

    32132132131

--- src/components/class/ShowAllLog.vue
+++ src/components/class/ShowAllLog.vue
@@ -58,7 +58,29 @@
                             <i class="el-icon-close"></i>
                         </div>
                         <div class="content">
-                            测试内容
+                            <el-row>
+                                <el-col :span="4">操作日期</el-col>
+                                <el-col :span="1">：</el-col>
+                                <el-col :span="19">2017/3/28</el-col>
+                            </el-row>
+                            <el-row>
+                                <el-col :span="4">操作类型</el-col>
+                                <el-col :span="1">：</el-col>
+                                <el-col :span="19">调课</el-col>
+                            </el-row>
+                            <el-row>
+                                <el-col :span="4">操作人</el-col>
+                                <el-col :span="1">：</el-col>
+                                <el-col :span="19">岳鹏举</el-col>
+                            </el-row>
+                            <el-row>
+                                <el-col :span="4">原因</el-col>
+                                <el-col :span="1">：</el-col>
+                                <el-col :span="19">
+                                    怒发冲冠，凭栏处、潇潇雨歇。抬望眼，仰天长啸，壮怀激烈。三十功名尘与土，八千里路云和月。莫等闲，白了少年头，空悲切！
+                                    靖康耻，犹未雪。臣子恨，何时灭！驾长车，踏破贺兰山缺。壮志饥餐胡虏肉，笑谈渴饮匈奴血。待从头、收拾旧山河，朝天阙。
+                                </el-col>
+                            </el-row>
                         </div>
                     </div>
                     <div class="dialog_mask" v-if="Dailog" @click="Close_mask"></div>
