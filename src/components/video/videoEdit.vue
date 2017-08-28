21b694541f29d6c1792241c2f643ace89802853f
sannycoo <413366383@qq.com>
2017-08-28 14:48:51 +0800

    zj Signed-off-by: sannycoo <413366383@qq.com>

--- src/components/video/videoEdit.vue
+++ src/components/video/videoEdit.vue
@@ -33,11 +33,24 @@
       <el-col :span="4">
         <div class="rt">视频缩略图：</div>
       </el-col>
-
+      <el-col :span="10">
+        <el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog v-model="dialogVisible" size="tiny">
+          <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
+      </el-col>
     </el-row>
     <el-row>
       <el-col :span="4">
         <div class="rt">可观看班级：</div>
+      </el-col>
+      <el-col :span="20">
+        <el-select v-model="value5" multiple placeholder="请选择" style="width: 80%">
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+          </el-option>
+        </el-select>
       </el-col>
     </el-row>
     <el-row>
@@ -45,7 +58,7 @@
         <div class="rt">视频描述简介：</div>
       </el-col>
       <el-col :span="20">
-        <quillEditor></quillEditor>
+        <quillEditor v-model="htmlTxt"></quillEditor>
       </el-col>
     </el-row>
   </div>
@@ -57,22 +70,25 @@
     data(){
       return{
         videoTitle:'',//视频名称
-        options:[{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }],
+        options:[{value: '选项1', label: '黄金糕'}, {value: '选项2', label: '双皮奶'}, {value: '选项3',label: '蚵仔煎'}],
         value:'',
-
+        value5:'',
+        dialogImageUrl: '',
+        dialogVisible: false,
+        htmlTxt:'',//富文本
       }
     },
     components:{
       quillEditor,
+    },
+    methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePictureCardPreview(file) {
+        this.dialogImageUrl = file.url;
+        this.dialogVisible = true;
+      }
     }
   }
 </script>
