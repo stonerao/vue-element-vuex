c1e00ffdd16149121c4889f0fa16c0a22ee4f78f
674656681@qq.com <674656681@qq.com>
2017-09-06 11:01:15 +0800

    r
    Signed-off-by: 674656681@qq.com <674656681@qq.com>

---
+++ src/components/operation/teacer_list.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span='15'>
+                <span class="line-height-36">
+                    <img src="../../assets/index/shuaxin.png" class="icon-img-xs marginleft5" @click="resh" />刷新-共1 条记录
+                </span>
+            </el-col>
+            <el-col :span="9">
+                <div class="float-right">
+                    <el-input placeholder="请输入试卷名称" icon="search" v-model="seach" :on-icon-click="seachClick" class="width150">
+                    </el-input>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            seach: ''
+        }
+    },
+    methods: {
+        seachClick() {
+
+        },
+        resh() {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    }
+}
+</script>
