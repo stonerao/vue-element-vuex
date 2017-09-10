30fc3cb1cbd7d4dac4959fca5c13e6bdfd8f28df
674656681@qq.com <674656681@qq.com>
2017-09-11 01:38:13 +0800

    r
    Signed-off-by: 674656681@qq.com <674656681@qq.com>

--- src/components/examination/alertExam.vue
+++ src/components/examination/alertExam.vue
@@ -7,10 +7,21 @@
             </p>
             <div>
                 <ul class="r-alert-list">
-                    <li><span>试卷名称：</span><span class="marginleft10">{{obj.e_title}}</span></li>
-                    <li><span>考试时长：</span><span class="marginleft10">{{obj.e_whenlong}}分钟</span></li>
-                    <li><span>卷面总分：</span><span class="marginleft10">{{obj.e_allsource}}</span></li> 
-                    <li><button @click="submit">开始考试</button></li> 
+                    <li>
+                        <span>试卷名称：</span>
+                        <span class="marginleft10">{{obj.e_title}}</span>
+                    </li>
+                    <li>
+                        <span>考试时长：</span>
+                        <span class="marginleft10">{{obj.e_whenlong}}分钟</span>
+                    </li>
+                    <li>
+                        <span>卷面总分：</span>
+                        <span class="marginleft10">{{obj.e_allsource}}</span>
+                    </li>
+                    <li>
+                        <button @click="submit">开始考试</button>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -25,17 +36,36 @@ export default {
 
         }
     },
-    created(){
+    created() {
         console.log(this.obj)
     },
-    methods:{
-        over(){
-            this.$emit("is_over",false)
+    methods: {
+        over() {
+            this.$emit("is_over", false)
         },
-        submit(){
-            // this.$emit("examgood",true)
-            if(this.obj.e_id)
-            this.$router.push({path:'/good_exam',query:{e_id:this.obj.e_id}})
+        submit() {
+            // this.$emit("examgood",true) 
+            if (parseInt(this.obj.e_endtime) * 1000 < Date.parse(new Date)) {
+                this.$notify({
+                    title: '警告',
+                    message: '考试已结束',
+                    type: 'warning'
+                });
+                return
+            } else if (parseInt(this.obj.e_starttime) * 1000 > Date.parse(new Date)) {
+                this.$notify({
+                    title: '警告',
+                    message: '考试暂未开始',
+                    type: 'warning'
+                });
+                return
+            }
+            else {
+                if (this.obj.e_id) {
+                    this.$router.push({ path: '/good_exam', query: { e_id: this.obj.e_id } })
+                }
+            }
+
         }
     }
 }
