b1ef1b9c1e6ca675853363b5221da8564b2c15ea
LTK <403182090@qq.com>
2017-09-21 14:43:07 +0800

    fdsg

--- src/view/search/search.vue
+++ src/view/search/search.vue
@@ -7,12 +7,33 @@
             </div>
             <div class="kd-box-content">
                 <description :prompts="prompts" @PromPts="promptsTem"></description>
-                <!--模块开始  -->
                 <div v-if="state==0">
-                    搜索站！！！！！
-                </div>
-                <div v-if="state==1">
-                    
+                	<div class="searchStation">
+                		<!-- 老师中心 -->
+                		<div v-if="DISTINGUISH == 2">
+                			<div class="Station_header">
+		                        <el-row>
+		                            <el-col :span="6">
+		                                <img src="../../assets/index/shuaxin.png" class="icon-img-xs" />刷新-共{{DataTotal}}条记录
+		                            </el-col>
+		                        </el-row>
+		                        <el-row class="clearfloat">
+	                                <el-col :span="5" style="float: right">
+	                                    <el-input placeholder="输入搜索关键词" v-model="SearchVal">
+	                                        <el-button slot="append" icon="search" @click.native="SearchStation"></el-button>
+	                                    </el-input>
+	                                </el-col>
+		                        </el-row>	
+                			</div>
+                			<div class="Station_body">
+                				
+                			</div>
+                		</div>
+                		<!-- 学生中心 -->
+                		<div v-if="DISTINGUISH == 3">
+                			
+                		</div>
+                	</div>
                 </div>
             </div>
             <bottomItem></bottomItem>
@@ -25,21 +46,31 @@ import titleItem from '@/components/main/title.vue'
 import titleActive from '@/components/main/titleActive.vue'
 import description from '@/components/main/description.vue'
 import bottomItem from '@/components/bottom/bottom.vue'
+import { getClass } from '@/utils/auth'
+import Info from '@/utils/l_axios1'
+
 export default {
     data() {
         return {
             titleItem: [
-                { name: "科目管理", index: 0 },
-                { name: "增加科目", index: 1 },
+                { name: "信息筛选", index: 0 },
             ],
             prompts: [
                 `该页面展示管理员的操作日志，可进行删除。`,
                 `侧边栏可以进行高级搜索`
             ],
-            state: 0, 
+            state: 0,
+            DISTINGUISH: getClass(),   //区分老师和学生
+            SearchVal: '',   //搜索值
+            DataTotal: 0,  // 数据总条数
         }
     },
     created() {
+        if(this.DISTINGUISH == 2){ //激活老师管理系统身份
+
+        }else if(this.DISTINGUISH == 3){ //学生
+
+        }
     },
     components: {
         titleItem, titleActive, description, bottomItem
@@ -53,6 +84,9 @@ export default {
         },
         promptsTem(status) {
             console.log(status)
+        },
+        SearchStation(){   //搜索请求接口
+
         }
     }
 }
