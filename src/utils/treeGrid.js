6d0fb6768cc32e31568dc191139e478361338f1d
LTK <403182090@qq.com>
2017-09-26 16:41:12 +0800

    修改！

--- src/utils/treeGrid.js
+++ src/utils/treeGrid.js
@@ -472,9 +472,7 @@ export default {
                             _show = res.data.data.department_info.special_tag;
                         this.className = _data.department_info.department_name;
                         this.teacherList = _data.teacher_list;
-                        this.selectVal={
-                            teacher: _data.department_info.head_teacher_id,
-                        }
+                        this.selectVal.teacher = _data.department_info.head_teacher_id;
                         if(parseInt(_show) == 3){
                             this.whetherShow = false;
                         }else{
