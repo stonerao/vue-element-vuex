9bf9ee697f96a07443c83a03af0f355d192dad45
LTK <403182090@qq.com>
2017-09-30 18:53:50 +0800

    asfsdaf

--- src/components/class/gradelist.vue
+++ src/components/class/gradelist.vue
@@ -70,7 +70,7 @@ export default {
             gradeParams: {
             	hasmore: true,
                 curpage: 1,//当前页数
-                one_pagenum: 10,
+                one_pagenum: 15,
                 page_count: 1,//总页数
                 total_num: 0
             },
@@ -101,7 +101,7 @@ export default {
             this.gradeParams = {
                 hasmore: true,
                 curpage: 1,//当前页数
-                one_pagenum: 10,
+                one_pagenum: 15,
                 page_count: 1,//总页数
                 total_num: 0
             };
